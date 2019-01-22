395a9779d2fb7650382f97d1f2b9773d56794dea
naresh.2050 <naresh.doosa@gmail.com>
2019-01-22 20:16:10 +0530

    commit

---
+++ FreeCrmBDD/src/stepDefinitions/LoginStepDefinition.java
@@ -0,0 +1,16 @@
+package stepDefinitions;
+
+import cucumber.api.java.en.Given;
+
+public class LoginStepDefinition {
+	
+	
+	@Given("^User is on Login Page$")
+	public void user_is_on_login_page() {
+		
+		System.out.println("Hello");
+		
+		
+	}
+
+}
